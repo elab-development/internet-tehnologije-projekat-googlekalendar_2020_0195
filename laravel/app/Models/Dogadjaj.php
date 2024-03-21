9305398d783074d6ca15576469fd952dc3ec66a2
milenkovica <aleksandra.mil2002@gmail.com>
2024-03-21 20:50:44 +0100

    kreiran i povezan model

---
+++ laravel/app/Models/Dogadjaj.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Dogadjaj extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'datum', 'vreme_od', 'vreme_do', 'naziv', 'opis', 'status', 'kategorija_id'
+    ];
+
+    // Odnos sa User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Odnos sa Kategorija
+    public function kategorija()
+    {
+        return $this->belongsTo(Kategorija::class);
+    }
+
+    // Odnos sa Reminder
+    public function reminders()
+    {
+        return $this->hasMany(Reminder::class);
+    }
+}
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -22,7 +22,10 @@ class User extends Authenticatable
         'email',
         'password',
     ];
-
+    public function dogadjaji()
+    {
+        return $this->hasMany(Dogadjaj::class);
+    }
     /**
      * The attributes that should be hidden for serialization.
      *
