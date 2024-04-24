d305339aa5e123599a172ea26fe98c7d182584f9
milenkovica <aleksandra.mil2002@gmail.com>
2024-04-24 11:29:59 +0200

    dodate rute pomocu react-router-dom

--- reactprojekat/src/App.js
+++ reactprojekat/src/App.js
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Login/Login';
 import Register from './Login/Register';
 
 function App() {
   return (
-    <div className="App">
-        <Login></Login>
-        <Register></Register>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
